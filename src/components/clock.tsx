fe5a25c25b0a5b7aa1ce0bc636bd039529ae071d
Roman Timoshkin <rmntim@yandex.ru>
2024-10-18 13:46:30 +0300

    fix: make clock client-side

--- src/components/clock.tsx
+++ src/components/clock.tsx
@@ -3,11 +3,12 @@
 import { useEffect, useState } from "react";
 
 export default function Clock() {
-    const [time, setTime] = useState<Date>(new Date());
+    const [time, setTime] = useState<string | null>(null);
 
     useEffect(() => {
+        setTime(new Date().toLocaleTimeString());
         const timerId = setInterval(() => {
-            setTime(new Date());
+            setTime(new Date().toLocaleTimeString());
         }, 1000);
 
         return () => clearInterval(timerId);
@@ -16,7 +17,7 @@ export default function Clock() {
     return (
         <div className="flex flex-col items-center justify-center">
             <p className="text-white">
-                Current Time: {time.toLocaleTimeString()}
+                Current Time: {time ?? "Loading..."}
             </p>
         </div>
     );
